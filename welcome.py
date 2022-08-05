5adab350dddba1f33592f25ee8883e35a1cdac81
Bright Olawale <oolatunjibright@yahoo.com>
2022-08-05 16:16:22 +0100

    [feat #5]: passing pycodestyle

---
+++ welcome.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def welcome():
+    name = input("What is your name?\n")
+    text = "have fun going through the program"
+    return "Welcome " + name + " to my Airbnb project, " + text + "!"
