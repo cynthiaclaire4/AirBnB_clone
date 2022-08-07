b49dafe1b56ea7e8fb77bd4e32b03734971be83a
cynthiaclaire4 <you@example.com>
2022-08-07 21:43:52 +0100

    [feat #6]: creating the console

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''console'''
+
+import cmd
+from datetime import datetime
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+class HBNBCommand(cmd.Cmd):
+    """ HBNB console """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
