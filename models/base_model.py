ec0c1e89e4337d2fbc8f8d1a9077bdb4285597c2
Bright Olawale <oolatunjibright@yahoo.com>
2022-08-08 18:17:15 +0100

    [fix #4]: basemodel file updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,41 @@
-from msilib.schema import Class
-import string
-from uuid import uuid4
+#!/usr/bin/env python3
+
 from datetime import datetime
+import models
 import uuid
 
 ''' Define class BaseModel'''
 
 class BaseModel:
-    id = string(uuid.uuid4())
-    created_at = datetime
-    updated_at = datetime
+    """"""
 
-    '''intializing the class'''
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        created_at = datetime.now()
-        updated_at = created_at
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
-def __str__(self):
-    """String representation of the BaseModel class"""
-    return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         '''updated at the current date time'''
         self.updated_at = datetime.now()
+        models.storage.save()
 
-    def to_dict(self)
-    '''returns all dict containing all keys/values of instance'''
-    new_dict = self.__dict__.copy()
-    return new_dict
-
-    
-
-
-
-
-
-        
-
-
-
-
+    def to_dict(self):
+        '''returns all dict containing all keys/values of instance'''
+        model_dict = dict(self.__dict__)
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        model_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return model_dict
 
