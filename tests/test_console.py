951cb9cee9b5fe4a7ee239cd73d765b64586ea41
Bright Olawale <oolatunjibright@yahoo.com>
2022-08-09 18:57:03 +0100

    [fix #11]: renamed test files

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,89 +2,58 @@
 """Test the console"""
 
 
-import unittest
-import models
-from console import HBNBCommand
-from io import StringIO
 import sys
+import models
+import unittest
+from io import StringIO
+from console import HBNBCommand
+from unittest.mock import create_autospec
 
 
-class TestConsole(unittest.TestCase):
-    """Test the console"""
-
-    def setup(self):
-        """setup"""
+class test_console(unittest.TestCase):
+    ''' Test the console module'''
+    def setUp(self):
+        '''setup for'''
         self.backup = sys.stdout
         self.capt_out = StringIO()
         sys.stdout = self.capt_out
 
-    def teardown(self):
-        """teardown"""
+    def tearDown(self):
+        ''''''
         sys.stdout = self.backup
 
     def create(self):
+        ''' create an instance of the HBNBCommand class'''
         return HBNBCommand()
 
-    def test_create(self):
-        """test create"""
-        hbnb = self.create()
-        self.assertIsInstance(hbnb, HBNBCommand)
+    def test_quit(self):
+        ''' Test quit exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("quit"))
 
-    def test_do_quit(self):
-        """test do quit"""
-        hbnb = self.create()
-        hbnb.do_quit('')
-        self.assertEqual(hbnb.do_quit(''), None)
+    def test_EOF(self):
+        ''' Test EOF exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("EOF"))
 
-    def test_do_EOF(self):
-        """test do EOF"""
-        hbnb = self.create()
-        hbnb.do_EOF('')
-        self.assertEqual(hbnb.do_EOF(''), None)
+    def test_all(self):
+        ''' Test all exists'''
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
 
-    def test_do_create(self):
-        """test do create"""
-        hbnb = self.create()
-        hbnb.do_create("BaseModel")
-        self.assertEqual(hbnb.do_create("BaseModel"), None)
-
-    def test_do_show(self):
-        """test do show"""
-        hbnb = self.create()
-        hbnb.do_create("BaseModel")
-        hbnb.do_show("BaseModel.1")
-        self.assertEqual(hbnb.do_show("BaseModel.1"), None)
-
-    def test_do_destroy(self):
-        """test do destroy"""
-        hbnb = self.create()
-        hbnb.do_create("BaseModel")
-        hbnb.do_destroy("BaseModel.1")
-        self.assertEqual(hbnb.do_destroy("BaseModel.1"), None)
-
-    def test_do_all(self):
-        """test do all"""
-        hbnb = self.create()
-        hbnb.do_create("BaseModel")
-        hbnb.do_all("BaseModel")
-        self.assertEqual(hbnb.do_all("BaseModel"), None)
-
-    def test_do_update(self):
-        """test do update"""
-        hbnb = self.create()
-        hbnb.do_create("BaseModel")
-        hbnb.do_update("BaseModel.1", "name", "Holberton")
-        self.assertEqual(hbnb.do_update("BaseModel.1", "name",
-                                        "Holberton"), None)
-
-    def test_do_count(self):
-        """test do count"""
-        hbnb = self.create()
-        hbnb.do_count("BaseModel")
-        self.assertEqual(hbnb.do_count("BaseModel"), None)
-
-    def test_do_show_count(self):
-        """test do show count"""
-        hbnb = self.create()
-        hbnb.do_show_count("BaseModel")
-        self.assertEqual(hbnb.do_show_count("BaseModel"), None)
+    def test_show(self):
+        '''
+            Testing that show exists
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User " + user_id)
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertTrue(str is type(x))
