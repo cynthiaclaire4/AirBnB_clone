2c98b90d28a5356a694e4c4624703cebc18efc3d
Bright Olawale <oolatunjibright@yahoo.com>
2022-08-09 10:38:01 +0100

    [feat #7]: Engine folder and it files added

---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""""""
+
+
+import models
+import json
+from models.user import User
+
+class FileStorage():
+    """"""
+    __file_path = "model_file.json"
+    __objects = dict()
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        my_dict = {}
+        for key, val in FileStorage.__objects.items():
+            my_dict[key] = val.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(my_dict, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
+
+
+
+
---
+++ models/engine/load_engine.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.engine.file_storage import FileStorage
+
+def load_engine():
+    storage_sys = FileStorage()
+    storage_sys.reload()
+    available_obj = storage_sys.all()
+    return available_obj
